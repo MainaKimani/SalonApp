2498bcd7f8e93d35effe4f53b17c635bfc0112d3
maina kimani <me@mainakimani.com>
2022-11-30 04:02:15 +0300

    register page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+
+function Register() {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
