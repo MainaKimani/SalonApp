d6e4cb088f2caf802acd1b9ec1657f71ab2077dd
maina kimani <me@mainakimani.com>
2022-11-30 04:01:54 +0300

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,14 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonPage } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import Header from './Header'
+import Login from './Login'
+
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+      {/* <Header /> */}
+      <Login />
     </IonPage>
   );
 };
