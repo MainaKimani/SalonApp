9a9a1df0d9b4ee7938c58f424ce640aecc8e7809
maina kimani <me@mainakimani.com>
2022-12-02 00:00:19 +0300

    firebase config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,159 @@
+import { IonPage, IonContent } from '@ionic/react';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, 
+         createUserWithEmailAndPassword,
+         signInWithEmailAndPassword,
+         GoogleAuthProvider,
+         signInWithPopup,
+         onAuthStateChanged,
+         updateProfile,
+         sendPasswordResetEmail  } from "firebase/auth";
+import Login from './pages/Login';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyArXPrIwlhp24cyVgEx4QWP0VvIlm8mIcI",
+    authDomain: "blend-parlor.firebaseapp.com",
+    projectId: "blend-parlor",
+    storageBucket: "blend-parlor.appspot.com",
+    messagingSenderId: "852223461821",
+    appId: "1:852223461821:web:91f28297ba1de7bbc8aad4",
+    measurementId: "G-FY30XBCS3T"
+  };
+  
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+
+//check if user is authenticated
+export function isAuthenticated(){
+    onAuthStateChanged(auth, (user) => {
+        var isAuth = true;
+        if (!user) {
+            isAuth = false;
+            console.log(isAuth)
+            return isAuth;
+          } else {
+            console.log(isAuth)
+            return isAuth;
+            }   
+      });
+}
+
+
+
+
+//login function
+export async function loginUser(email: any, password: any){
+    try{   
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                // ...
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.error(errorMessage)
+                const email_warning:any = document.getElementById('email_warning');
+                const password_warning:any = document.getElementById('password_warning');
+                const login_warning:any = document.getElementById('login_warning');
+                if (errorMessage==='Firebase: Error (auth/user-not-found).'){
+                    email_warning.innerHTML='Email not found';
+                }
+                if (errorMessage==='Firebase: Error (auth/invalid-email).'){
+                    email_warning.innerHTML='Invalid email';
+                }
+                if (errorMessage==='Firebase: Error (auth/wrong-password).'){
+                    password_warning.innerHTML='Incorrect password';
+                }
+                if (errorMessage==='Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).'){
+                    const errMsg = errorMessage.slice(9, -25)
+                    login_warning.innerHTML=errMsg;
+                }
+          });
+        } 
+    catch (error) {
+        console.error(error)
+    }
+}
+
+//sign up
+export async function signinUser(fname: any, lname: any, email: any, password: any){
+    try{
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+              // Signed in 
+              const user = userCredential.user;
+
+              updateProfile(user, {
+                displayName: fname+" "+lname }).then(() => {
+                    const name = user.displayName;
+                    console.log(name);
+                // ...
+              }).catch((error)=>{
+                const errorMessage:any = error.code;
+                console.log(errorMessage);
+              })
+            .catch((error) => {
+              const errorMessage:any = error.message;
+              if (errorMessage ==="Firebase: Error (auth/email-already-in-use)."){
+                const password_email:any = document.getElementById('email_warning');
+                password_email.innerHTML='Email already in use';
+              }
+              else{
+                alert('Opps! Something bad happened. Please try again');
+              }
+              console.log(errorMessage);
+            });
+
+            console.log(user);
+        });
+    } 
+    catch (error) {
+        console.error(error)
+    }
+}
+
+//Google sign in
+export function googleSignIn (){
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user);
+    }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+        console.log(errorMessage)
+    });
+}
+
+//Resetting password
+export function verifyEmail(){
+    const email = 'kalexmaina@gmail.com';
+    sendPasswordResetEmail(auth, email)
+  .then(() => {
+    console.log('email sent - password reset')
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error(errorMessage)
+    // ..
+  });
+}
