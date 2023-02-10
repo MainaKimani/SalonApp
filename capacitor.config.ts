94e0ca917f0273942d591e52ec2035b37486ea82
maina kimani <me@mainakimani.com>
2023-02-10 19:28:55 +0300

    build config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'salonApp',
   appName: 'salonApp',
   webDir: 'build',
   bundledWebRuntime: false
